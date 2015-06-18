ede9fd03f48aad5243aa83a057276f1105c3878a
zhangtaichao <taichao1988@foxmail.com>
2015-06-18 15:06:49 +0800

    init

---
+++ common_mgmt/src/main/java/net/shinc/CommonMgmtApplication.java
@@ -0,0 +1,12 @@
+package net.shinc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonMgmtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonMgmtApplication.class, args);
+    }
+}
